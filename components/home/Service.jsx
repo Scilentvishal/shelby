3f5868feb950d2700f1594437cacc97ac95e67a2
Vishal Rahangdale <vishalrahangdale14@gmail.com>
2023-06-06 17:41:41 +0530

    second commit

--- components/home/Service.jsx
+++ components/home/Service.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
