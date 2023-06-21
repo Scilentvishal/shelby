5f53b632afcc6f6099f21a5d29c3a56cbbd71187
Vishal Rahangdale <vishalrahangdale14@gmail.com>
2023-06-21 16:58:21 +0530

    contact form padding

--- pages/book.js
+++ pages/book.js
@@ -26,7 +26,7 @@ const book = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 p-6 md:mt-0">
                             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
                             <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
                             <div className="relative mb-4">
